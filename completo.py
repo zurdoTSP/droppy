317db5083afafe5a614a1dc5396b3f0bc27ac464
Antonio J Campos Gámez <antoniozprograma@gmail.com>
2017-02-03 16:05:24 +0100

    primer commit

---
+++ completo.py
@@ -0,0 +1,117 @@
+import dropbox
+import os
+import sys
+import webbrowser
+from colores import bcolors
+import ficheros
+########################################################################
+class DropObj(object):
+	"""
+	Dropbox object that can access your dropbox folder,
+	as well as download and upload files to dropbox
+	"""
+
+	#----------------------------------------------------------------------
+	def __init__(self, filename=None, path='/'):
+		"""Constructor"""
+		self.base_path = os.path.dirname(os.path.abspath(__file__))
+		self.filename = filename
+		self.path= path
+		self.app_key= 'wgc9jk089m26mvy'
+		self.app_secret= '4z25ev9bejzz0ae'
+		self.app_type = 'app_folder'
+		self.flow = dropbox.client.DropboxOAuth2FlowNoRedirect(self.app_key, self.app_secret)
+		self.authorize_url= self.flow.start()
+		self.client = None
+ #V6rPWlcOs5sAAAAAAACXUtecuxDNv6vAceKjqGoNnVfBNhKkwGpdcfWtPa6k0b_X
+
+	#----------------------------------------------------------------------
+	def nuevoToken(self,code):
+		"""
+		Connect and authenticate with dropbox
+		"""
+		self.access_token, user_id = self.flow.finish(code)
+		self.client = dropbox.client.DropboxClient(self.access_token)
+		outfile = open('.token.txt', 'w') # Indicamos el valor 'w'.
+		outfile.write(self.access_token)
+		outfile.close()
+
+	#----------------------------------------------------------------------
+	def autoiden(self):
+		"""
+		Connect and authenticate with dropbox
+		"""
+		infile = open('.token.txt', 'r')
+		x=infile.read()
+		infile.close()
+		self.client = dropbox.client.DropboxClient(x)
+
+		#----------------------------------------------------------------------
+	def getAutorize(self):
+
+		return self.authorize_url
+
+		#----------------------------------------------------------------------
+	def download_file(self, filename=None, outDir=None):
+		"""
+		Download either the file passed to the class or the file passed
+		to the method
+		"""
+
+		if filename:
+			fname = filename
+			f, metadata = self.client.get_file_and_metadata("/" + fname)
+		else:
+			fname = self.filename
+			f, metadata = self.client.get_file_and_metadata("/" + fname)
+
+		if outDir:
+			dst = os.path.join(outDir, fname)
+		else:
+			dst = fname
+
+		with open(fname, "w") as fh:
+			fh.write(f.read())
+
+		return dst, metadata
+
+	#----------------------------------------------------------------------
+	def get_account_info(self):
+
+		return self.client.account_info()
+
+	#----------------------------------------------------------------------
+
+	def upload_file(self,ruta="/"):
+		"""
+		Subida de fichero a dropbox con ruta opcional
+		"""
+		try:
+			with open(self.filename) as fh:
+				path = os.path.join(self.path, self.filename)
+				res = self.client.put_file(ruta+self.filename, fh,True)
+				print ("uploaded: ",self.filename)
+		except Exception:
+			print ("ERROR: ")
+
+		return res
+
+	#----------------------------------------------------------------------
+	def listarCarpetas(self):
+		a=ficheros.ficheros()
+		metadata = self.client.metadata('/')
+		for x in metadata["contents"]:
+			print(bcolors.nuevo+x['path']+bcolors.ENDC)
+			if x['is_dir']==True:
+				a.setNDir(x['path'])
+				metadata2 = self.client.metadata(x['path'])
+				for x2 in metadata2["contents"]:
+					a.setNSecundario(x2['path'])
+					print("\t"+bcolors.morado+x2['path']+bcolors.ENDC)
+		return a
+	#----------------------------------------------------------------------
+
+#if __name__ == "__main__":
+#	drop = DropObj("config.ini")
+#	drop.listarCarpetas()
+#	drop.upload_file("prueba/")
---
+++ pruebas.py
@@ -0,0 +1,61 @@
+import sys
+from PyQt5.QtWidgets import *
+from PyQt5 import uic
+from PyQt5.QtGui import *
+from PyQt5.QtCore import Qt
+import ctypes #GetSystemMetrics
+import completo
+import os.path
+#Clase heredada de QMainWindow (Constructor de ventanas)
+
+
+  #Asignar estilos CSS
+  #self.setStyleSheet("background-color: #000; color: #fff;")
+
+
+class MainWindow(QMainWindow):
+ #Método constructor de la clase
+	def __init__(self,dr):
+  #Iniciar el objeto QMainWindow
+		QMainWindow.__init__(self)
+  #Cargar la configuración del archivo .ui en el objeto
+		uic.loadUi("mainwindow2.ui", self)
+		self.drop = dr
+		self.setStyleSheet("background-color:#0099FF; color: #fff;font-size:bold")
+		self.treeWidget.setStyleSheet("background-color:#0099FF; color: #fff;font-size:bold")
+		self.formar()
+
+	def formar(self):
+
+		t=self.drop.listarCarpetas()
+
+		t2=t.getDirectorios()
+		hijos=t.getHijos()
+		header=QTreeWidgetItem(["Tree"])
+		#header.setBackgroundColor(0, QtGui.QColor('green'))
+		icon=QIcon('home-icon.png')
+		icon2=QIcon('text-plain-icon.png')
+		self.treeWidget.setHeaderItem(header)   #Another alternative is setHeaderLabels(["Tree","First",...])
+		root = QTreeWidgetItem(self.treeWidget, ["dropbox"])
+		for i in range(len(t2)):
+			q=[]
+			q.append(t2[i])
+			A = QTreeWidgetItem(root,q)
+			A.setIcon(0,icon)
+			for j in range(len(hijos)):
+				#print(hijos[j].getNombre())
+				if ("/"+hijos[j].getPadre())==t2[i]:
+					q=[]
+					q.append(hijos[j].getNombre())
+					barA = QTreeWidgetItem(A,q)
+					barA.setIcon(0,icon2)
+
+"""
+
+barA = QTreeWidgetItem(A, ["bar", "i"])
+bazA = QTreeWidgetItem(A, ["baz", "a"])
+
+self.treeWidget.setEditTriggers(self.treeWidget.NoEditTriggers)
+
+self.treeWidget.itemDoubleClicked.connect(self.checkEdit)
+"""
