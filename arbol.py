109933996acbcd071488881da16acdd44a385e21
Antonio Javier Campos Gámez <antoniozprograma@gmail.com>
2017-05-15 11:49:42 +0200

    iconos añadidos

--- arbol.py
+++ arbol.py
@@ -5,7 +5,7 @@ from PyQt5.QtGui import *
 from PyQt5 import QtGui
 from PyQt5.QtCore import Qt
 from PyQt5 import QtPrintSupport
-
+import os.path
 #Clase	heredada	de	QMainWindow	(Constructor	de	ventanas)
 class Arbol(QMainWindow):
 	#Método	constructor	de	la	clase
@@ -13,16 +13,19 @@ class Arbol(QMainWindow):
 		#Iniciar	el	objeto	QMainWindow
 		QMainWindow.__init__(self)
 		#Cargar	la	configuración	del	archivo	.ui	en	el	objeto
+		ruta=os.getcwd()+"/icons/"
 		uic.loadUi("mainwindow2.ui",self)
 		self.drop=dx
-		self.setWindowTitle("Cambiando	el	título	de	la	ventana")
-#		iconCar=QIcon(ruta+'New-Folder-icon.png')
-#		iconFil=QIcon(ruta+'nfile.png')
-#		iconBor=QIcon(ruta+'papelera.png')
-		
+		self.setWindowTitle("Droppy")
+		iconCar=QIcon(ruta+'New-Folder-icon.png')
+		iconFil=QIcon(ruta+'nfile.png')
+		iconBor=QIcon(ruta+'papelera.png')
+		self.bCarpeta.setIcon(iconCar)
+		self.bFichero.setIcon(iconFil)
+		self.bBorrar.setIcon(iconBor)
 		self.carpetas.itemClicked.connect(self.hijos)
 		self.forma()
-		
+
 	def forma(self):
 		iconCar=QIcon('New-Folder-icon.png')
 		self.directorio=self.drop.listarCarpetas()
@@ -37,10 +40,14 @@ class Arbol(QMainWindow):
 		x=self.carpetas.currentItem().text()
 		for i in self.directorio:
 			if x==i.getNombre():
-				for j in i.getHijo():	
-					item=QListWidgetItem(j)
+				for j in i.getHijo():
+					item=QListWidgetItem(self.convertir(j))
 					item.setIcon(iconCar)
 					self.ficheros.addItem(item)
-		print(x)
-		
+	def convertir(self,cad):
+		lista=cad.split('/')
+		return(lista[2])
 
+		print(x)
+		def fname(arg):
+			pass
