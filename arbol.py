e06c6f669cd59d2246067fe6e427739019226758
Antonio Javier Campos Gámez <antoniozprograma@gmail.com>
2017-05-20 12:18:00 +0200

    añadido creación de carpetas

--- arbol.py
+++ arbol.py
@@ -13,41 +13,72 @@ class Arbol(QMainWindow):
 		#Iniciar	el	objeto	QMainWindow
 		QMainWindow.__init__(self)
 		#Cargar	la	configuración	del	archivo	.ui	en	el	objeto
-		ruta=os.getcwd()+"/icons/"
+		self.ruta=os.getcwd()+"/icons/"
 		uic.loadUi("mainwindow2.ui",self)
+		self.carpetaActual=""
 		self.drop=dx
 		self.setWindowTitle("Droppy")
-		iconCar=QIcon(ruta+'New-Folder-icon.png')
-		iconFil=QIcon(ruta+'nfile.png')
-		iconBor=QIcon(ruta+'papelera.png')
+		iconCar=QIcon(self.ruta+'New-Folder-icon.png')
+		iconFil=QIcon(self.ruta+'nfile.png')
+		iconBor=QIcon(self.ruta+'papelera.png')
 		self.bCarpeta.setIcon(iconCar)
 		self.bFichero.setIcon(iconFil)
 		self.bBorrar.setIcon(iconBor)
 		self.carpetas.itemClicked.connect(self.hijos)
 		self.forma()
+		#Asociar botones a funciones
+		self.bCarpeta.clicked.connect(self.crearFolder)
+	#----------------------------------------------------------------------
 
 	def forma(self):
-		iconCar=QIcon('New-Folder-icon.png')
+		"""
+		Función que rellena la lista que contiene las carpetas
+		"""
+		iconCar=QIcon(self.ruta+'home-icon.png')
 		self.directorio=self.drop.listarCarpetas()
 		for x in self.directorio:
 			item=QListWidgetItem(x.getNombre())
 			item.setIcon(iconCar)
 			self.carpetas.addItem(item)
+	#----------------------------------------------------------------------
 
 	def hijos(self):
+		"""
+		Función que rellena el arbol de ficheros según su carpeta contenedora
+		"""
 		self.ficheros.clear()
-		iconCar=QIcon('text-plain-icon.png')
+		iconCar=QIcon(self.ruta+'text-plain-icon.png')
 		x=self.carpetas.currentItem().text()
+		self.carpetaActual=x
 		for i in self.directorio:
 			if x==i.getNombre():
 				for j in i.getHijo():
 					item=QListWidgetItem(self.convertir(j))
 					item.setIcon(iconCar)
 					self.ficheros.addItem(item)
+		#----------------------------------------------------------------------
+
 	def convertir(self,cad):
+		"""
+		Función que elimina el directorio de la cadena de los hijos
+		"""
 		lista=cad.split('/')
 		return(lista[2])
-
 		print(x)
 		def fname(arg):
 			pass
+#----------------------------------------------------------------------
+
+	def crearFolder(self):
+		"""
+		Función para crear carpetas en Dropbox.
+		"""
+		value,crear= QInputDialog.getText(self, "crear archivo", "Nombre de la nueva carpeta:")
+		if crear and value!='':
+			print('Nombre:', value)
+			self.drop.crearCarpeta(value)
+			iconCar=QIcon(self.ruta+'home-icon.png')
+			item=QListWidgetItem("/"+value)
+			item.setIcon(iconCar)
+			self.carpetas.addItem(item)
+#----------------------------------------------------------------------
